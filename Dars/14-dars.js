5096d28c8b671e28dffb6f0fdf2e7701f14b9bbc
amirbek <amonovamirbek1@gmail.com>
2024-04-15 12:38:59 +0500

    A

--- Dars/14-dars.js
+++ Dars/14-dars.js
@@ -24,7 +24,7 @@ console.log(name, age, location);
 
 function Indeks(str) {
     if (str.length > 3) {
-        const [,,,a] = str
+        const [, , , a] = str
         return a
     } else {
         return "Null"
@@ -32,4 +32,53 @@ function Indeks(str) {
 }
 let str = "salom";
 console.log(Indeks(str));
+
+
+
+
+
+
+const users = [
+    {
+        intro: {
+            fistname: "Solih",
+            lastname: "Abdullayev",
+        },
+        age: 24,
+        addess: ["Toshkent, Yashnabod", "Samarqand, Mikrayon"],
+        contact: {
+            email: "samad@gmail.com",
+            social: {
+                telegram: "@samad001",
+                instagram: "@samad1k",
+            },
+            phone: ["+99 899 811 22 10", "+99 899 811 22 12"],
+        },
+        extra_info: {
+            father: "Halim Abdullayev",
+        },
+    },
+    {
+        intro: {
+            fistname: "Jamol",
+        },
+        contact: {
+            email: "jamol@gmail.com",
+            phone: ["+99 890 500 11 11"],
+        },
+        addess: ["Toshkent, Yunusobod"],
+    },
+];
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].addess[1]);
+}
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].contact.social?.telegram);  
+}
+
+
+
+
+
 
--- Dars/tempCodeRunnerFile.js
+++ Dars/tempCodeRunnerFile.js
@@ -1,11 +1,43 @@
 
-function Indeks(str) {
-    if (str.length > 3) {
-        const [,,,a] = str
-        return a
-    } else {
-        return "Null"
-    }
+
+
+
+const users = [
+    {
+        intro: {
+            fistname: "Solih",
+            lastname: "Abdullayev",
+        },
+        age: 24,
+        addess: ["Toshkent, Yashnabod", "Samarqand, Mikrayon"],
+        contact: {
+            email: "samad@gmail.com",
+            social: {
+                telegram: "@samad001",
+                instagram: "@samad1k",
+            },
+            phone: ["+99 899 811 22 10", "+99 899 811 22 12"],
+        },
+        extra_info: {
+            father: "Halim Abdullayev",
+        },
+    },
+    {
+        intro: {
+            fistname: "Jamol",
+        },
+        contact: {
+            email: "jamol@gmail.com",
+            phone: ["+99 890 500 11 11"],
+        },
+        addess: ["Toshkent, Yunusobod"],
+    },
+];
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].addess[1]);
 }
-let str = "salom";
-console.log(Indeks(str));
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i]?.contact.social.telegram);  
+}
+
